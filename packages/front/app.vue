d84a94bc98dddbb813d45b1d35e0b51ddac9f02c
Przemek Wojszwiłło <p.wojszwillo@macopedia.com>
2023-06-23 11:42:20 +0200

    Add officeId link to main menu

--- packages/front/app.vue
+++ packages/front/app.vue
@@ -4,6 +4,7 @@
       <li><NuxtLink to="/">INDEX</NuxtLink></li>
       <li><NuxtLink to="/contractors">/contractors</NuxtLink></li>
       <li><NuxtLink to="/contractors/123123123">/contractors/clientId</NuxtLink></li>
+      <li><NuxtLink to="/contractors/123123123/2234">/contractors/clientId/officeId</NuxtLink></li>
     </ul>
   </div>
   <NuxtPage />
